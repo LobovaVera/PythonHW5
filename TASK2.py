164dc6d08c4b2b7b609b3aa9d2e9cb72f9e697ff
Vera Lobova <pchilli@yandex.ru>
2023-01-14 21:51:48 +0300

    initital commit

---
+++ TASK2.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+num_1 = False
+num_2 = False
+num_3 = False
+num_4 = False
+num_5 = False
+num_6 = False
+num_7 = False
+num_8 = False
+num_9 = False
+
+
+
+win = tk.Tk()
+win.title("Крестики-нолики")
+win.geometry("500x500+100+100")
+win.resizable(False, False)
+win.mainloop()
