0d54a64eb9fbb565ee413d3468553fed9852614e
Vera Lobova <pchilli@yandex.ru>
2023-01-17 12:57:11 +0300

    try to make labels

--- TASK2.py
+++ TASK2.py
@@ -1,42 +1,115 @@
 # Создайте программу для игры в 'Крестики-нолики'
 # НЕОБЯЗАТЕЛЬНО Добавить игру против бота с интеллектом
-
+import sys
 import tkinter as tk
+from tkinter import *
+import time
 
-def game():
-    symbol = ''
-
-    num_1 = False
-    num_2 = False
-    num_3 = False
-    num_4 = False
-    num_5 = False
-    num_6 = False
-    num_7 = False
-    num_8 = False
-    num_9 = False
+count = 2
+score_list = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+text = ''
+win_text = ''
 
 
+win_list = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
+], [2,4,6]]
 
+def check_the_winner(score_list):
+    print(score_list)
+    # win_text = StringVar()
+    for i in range(0, len(win_list)):
+        if score_list[win_list[i][0]] == score_list[win_list[i][1]] == score_list[win_list[i][2]] == 1:
+            print("WIN 0")
+            # win_text.set("Выйграл О")
+            # win.update_idletasks()
+            label1.config(text = "Выйграл О")
+            label1.grid()
+            time.sleep(5)
+            # sys. exit()
+        elif score_list[win_list[i][0]] == score_list[win_list[i][1]] == score_list[win_list[i][2]] == 2:
+            print("win x")
+            # win_text.set("Выйграл X")
+            label1.config(text = "Выйграл X")
+            label1.grid()
+            time.sleep(5)
+            # sys. exit()
+        else:
+            print("CONTINUE")
+            
+            if any(x == 0 for x in score_list):
+                print( "if CONTINUE")
+                label1.config(text = "Игра продолжается")
+                label1.grid()
+                   
+            else:
+                print( "Ничья")
+                label1.config(text = "Ничья_")
+                label1.grid()
+                time.sleep(5)
+                sys.exit()
+            
+
+   
 
 
 win = tk.Tk()
 win.title("Крестики-нолики")
-# win.geometry("500x500+100+100")
-win.resizable(False, False)
 
+win.resizable(0, 0)
 
+def score(n):
+    global text
+    global count
+    score_list[n] = count
+    if count == 1:
+        text= 'O'
+        count = 2
+    else:
+        text = 'X'
+        count = 1
+    match n:
+        case 0:
+            btn1['text'] = f'{text}'
+        case 1:
+            btn2['text'] = f'{text}'
+        case 2:
+            btn3['text'] = f'{text}'
+        case 3:
+            btn4['text'] = f'{text}'
+        case 4:
+            btn5['text'] = f'{text}'
+        case 5:
+            btn6['text'] = f'{text}'
+        case 6:
+            btn7['text'] = f'{text}'
+        case 7:
+            btn8['text'] = f'{text}'
+        case 8:
+            btn9['text'] = f'{text}'
+        
 
-btn1 = tk.Button(win, text = f'').grid(row = 0, column = 0, stick='wens')
-btn2 = tk.Button(win, text = f'').grid(row = 0, column = 1, stick='wens')
-btn3 = tk.Button(win, text = f'').grid(row = 0, column = 2, stick='wens')
-btn4 = tk.Button(win, text = f'').grid(row = 1, column = 0, stick='wens')
-btn5 = tk.Button(win, text = f'').grid(row = 1, column = 1, stick='wens')
-btn6 = tk.Button(win, text = f'').grid(row = 1, column = 2, stick='wens')
-btn7 = tk.Button(win, text = f'').grid(row = 2, column = 0, stick='wens')
-btn8 = tk.Button(win, text = f'').grid(row = 2, column = 1, stick='wens')
-btn9 = tk.Button(win, text = f'').grid(row = 2, column = 2, stick='wens')
+btn1 = tk.Button(win, text=f'{text}', command=lambda: [score(0), check_the_winner(score_list)] )
+btn2 = tk.Button(win, text=f'{text}', command=lambda: [score(1), check_the_winner(score_list)] )
+btn3 = tk.Button(win, text=f'{text}', command=lambda: [score(2), check_the_winner(score_list)] )
+btn4 = tk.Button(win, text=f'{text}', command=lambda: [score(3), check_the_winner(score_list)] )
+btn5 = tk.Button(win, text=f'{text}', command=lambda: [score(4), check_the_winner(score_list)] )
+btn6 = tk.Button(win, text=f'{text}', command=lambda: [score(5), check_the_winner(score_list)] )
+btn7 = tk.Button(win, text=f'{text}', command=lambda: [score(6), check_the_winner(score_list)] )
+btn8 = tk.Button(win, text=f'{text}', command=lambda: [score(7), check_the_winner(score_list)] )
+btn9 = tk.Button(win, text=f'{text}', command=lambda: [score(8), check_the_winner(score_list)] )
 
+btn1.grid(row=0, column=0, stick='wens')
+btn2.grid(row=0, column=1, stick='wens')
+btn3.grid(row=0, column=2, stick='wens')
+btn4.grid(row=1, column=0, stick='wens')
+btn5.grid(row=1, column=1, stick='wens')
+btn6.grid(row=1, column=2, stick='wens')
+btn7.grid(row=2, column=0, stick='wens')
+btn8.grid(row=2, column=1, stick='wens')
+btn9.grid(row=2, column=2, stick='wens')
+
+label1 = tk.Label( win, text = "Победит сильнейший", textvariable = win_text, font=('Calibri 15'))
+label1.grid()
 
 
 win.grid_columnconfigure(0, minsize=200)
@@ -50,14 +123,14 @@ win.mainloop()
 # графическое оформление
 # win = tk.Tk()
 # photo = tk.PhotoImage(file = 'candy2.png')
-# win.iconphoto(False, photo)
+# win.iconphoto(0, photo)
 # win.config( bg = '#F5CAF2')
 # win.title("Конфетки")
 # win.geometry("500x500+100+100")
-# win.resizable(False, False)
+# win.resizable(0, 0)
 
 # btn1 = tk.Button(win,
-# text = "Играем вдвоем",
+# f'{text}'= "Играем вдвоем",
 # bg = "pink",
 # font = ('Arial', 15, 'bold'),
 # relief = tk.RAISED,
@@ -67,7 +140,7 @@ win.mainloop()
 # btn1.pack()
 
 # btn2 = tk.Button(win,
-# text = "Игра против бота",
+# f'{text}'= "Игра против бота",
 # bg = "pink",
 # font = ('Arial', 15, 'bold'),
 # relief = tk.RAISED,
--- TESTTASK.py
+++ TESTTASK.py
@@ -1,31 +1,71 @@
-# Import the required libraries
+# # Import the required libraries
+# from tkinter import *
+
+# # Create an instance of tkinter frame or window
+# win=Tk()
+
+# # Set the size of the tkinter window
+# win.geometry("700x350")
+
+# def cal_sum():
+#    t1=int(a.get())
+#    t2=int(b.get())
+#    sum=t1+t2
+#    label.config(text=sum)
+#    if t1<t2:
+#     print("hi")
+
+# # Create an Entry widget
+# Label(win, text="Enter First Number", font=('Calibri 10')).pack()
+# a=Entry(win, width=35)
+# a.pack()
+# Label(win, text="Enter Second Number", font=('Calibri 10')).pack()
+# b=Entry(win, width=35)
+# b.pack()
+
+# label=Label(win, text="Total Sum : ", font=('Calibri 15'))
+# label.pack(pady=20)
+
+# Button(win, text="Calculate Sum", command=cal_sum).pack()
+
+# win.mainloop()
+# import tkinter as tk
+# from tkinter import *
+
+# win=tk.Tk()
+# count = 1
+# score_list = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# text = ''
+# def score(a):
+#     # score_list[n] = 1
+#     # if count == 1:
+#     #     text = 'O'
+#     #     count = 2
+#     # else:
+#     #     text = 'X'
+#     #     count = 1
+#     # match n:
+#     #     case 0:
+#     global text
+#     text += a 
+#     btn1['text'] = f'{text}'
+
+# btn1 = tk.Button(win, text =  f'{text}', command = lambda: score('1') )
+# btn1.grid(row = 0, column = 0, stick='wens')
+
+# win.mainloop()
+
 from tkinter import *
 
-# Create an instance of tkinter frame or window
-win=Tk()
+root = Tk()
+var = StringVar()
+var.set('hello')
 
-# Set the size of the tkinter window
-win.geometry("700x350")
+l = Label(root, textvariable = var)
+l.pack()
 
-def cal_sum():
-   t1=int(a.get())
-   t2=int(b.get())
-   sum=t1+t2
-   label.config(text=sum)
-   if t1<t2:
-    print("hi")
+t = Entry(root, textvariable = var)
+t.pack()
 
-# Create an Entry widget
-Label(win, text="Enter First Number", font=('Calibri 10')).pack()
-a=Entry(win, width=35)
-a.pack()
-Label(win, text="Enter Second Number", font=('Calibri 10')).pack()
-b=Entry(win, width=35)
-b.pack()
-
-label=Label(win, text="Total Sum : ", font=('Calibri 15'))
-label.pack(pady=20)
-
-Button(win, text="Calculate Sum", command=cal_sum).pack()
-
-win.mainloop()
+root.mainloop()
