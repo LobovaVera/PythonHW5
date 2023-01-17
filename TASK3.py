82daebd9556790f73e3d922ece4868a20840033c
Vera Lobova <pchilli@yandex.ru>
2023-01-17 20:18:58 +0300

    last change

--- TASK3.py
+++ TASK3.py
@@ -3,30 +3,51 @@
 # aaaaabbbcccc -> 5a3b4c
 # 5a3b4c -> aaaaabbbcccc
 #НЕКОРРЕКТНО РАБОТАЕТ С НЕВЕРНЫМ ВВОДОМ!
+our_string = ''
+path = r'text.txt'
 
-our_string = input("Введите символы: ")
+data = open(path, 'r', encoding = 'UTF-8')
+# for el in data:
+#     our_string += el
+
+our_string = data.readline()
 res_string = ""
-try:
-    int(our_string[0])
-    print("Восстанавливаем")
-    for i in range(0, len(our_string)):
-        try:
-            for j in range(0,int(our_string[i])):
-                print(our_string[i+1], end = '')
-        except:
-            continue
+# print(our_string)
+data.close()
 
-except:
-    print("Сжимаем")
-    count = 1
-    j = 0
-    i = 0
-    for i in range(1,len(our_string)):
-        if our_string[i] == our_string[i-1]:
-            count += 1
-         
-        else:
-            print(str(count) + our_string[i-1], end = '')
-            count = 1
+path = r'text2.txt'
 
-    print(str(count) + our_string[i])
+data = open(path, 'r', encoding = 'UTF-8')
+our_string2 = data.readline()
+data.close()
+
+
+
+def zip_unzip(our_string):
+    try:
+        int(our_string[0])
+        print("Восстанавливаем")
+        for i in range(0, len(our_string)):
+            try:
+                for j in range(0,int(our_string[i])):
+                    print(our_string[i+1], end = '')
+            except:
+                continue
+
+    except:
+        print("Сжимаем")
+        count = 1
+        j = 0
+        i = 0
+        for i in range(1,len(our_string)):
+            if our_string[i] == our_string[i-1]:
+                count += 1
+            
+            else:
+                print(str(count) + our_string[i-1], end = '')
+                count = 1
+
+        print(str(count) + our_string[i])
+
+zip_unzip(our_string)
+zip_unzip(our_string2)
